3858050f7c3cea6bc7c7a6d01b9d5f328e37feef
talitapereira <94771037+talitapereira@users.noreply.github.com>
2023-11-16 19:24:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import{View,Text} from 'react-native';
+import HomePage from './src/components/pages/HomePage'
+
+const App =() => {
+
+  return(
+
+    <HomePage/>
+    
+  );
+
+}
+
+export default App;
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import{View,Text} from 'react-native';
+
+const HomePage =() => {
+
+  return(
+
+    <View style={{flex:1, alignItems:'center',justifyContent:'center'}}> 
+    
+      <Text> PUC Minas </Text>
+
+    </View>
+    
+  );
+
+}
+
+export default HomePage;
+
+
